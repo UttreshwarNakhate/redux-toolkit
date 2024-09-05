4f6770239d13e20ad7f970a2854a05329ea8bfc2
un-the-dev <104813812+UttreshwarNakhate@users.noreply.github.com>
2024-09-05 10:43:50 +0530

    createAsyncThunk

--- src/store/productSclice.js
+++ src/store/productSclice.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 export const STATUSES = Object.freeze({
   IDLE: "idle",
@@ -13,14 +13,28 @@ const productSlice = createSlice({
     status: STATUSES.IDLE,
   },
 
-  reducers: {
-    setProducts(state, action) {
-      state.data = action.payload;
-    },
-    setStatus(state, action) {
-      state.status = action.payload;
-    },
-  },
+  // reducers: {
+  //   setProducts(state, action) {
+  //     state.data = action.payload;
+  //   },
+  //   setStatus(state, action) {
+  //     state.status = action.payload;
+  //   },
+  // },
+
+  extraReducers:(builder)=>{
+    builder
+    .addCase(fetchProducts.pending, (state, action)=>{
+      state.status = STATUSES.LOADING
+    })
+    .addCase(fetchProducts.fulfilled, (state, action)=>{
+      state.data = action.payload
+      state.status = STATUSES.IDLE
+    })
+    .addCase(fetchProducts.rejected, (state, action)=>{
+      state.status = STATUSES.ERROR
+    })
+  }
 });
 
 export const { setProducts, setStatus } = productSlice.actions;
@@ -28,16 +42,19 @@ export const { setProducts, setStatus } = productSlice.actions;
 export default productSlice.reducer;
 
 // Thunks
-
-
+export const fetchProducts = createAsyncThunk("products/fetch", async () => {
+  const res = await fetch("https://fakestoreapi.com/products");
+  const data = await res.json();
+  return data;
+});
 
 // export function fetchProducts() {
 //   return async function fetchProductThunk(dispatch, getState) {
 //     // Apply first loading state
 //     dispatch(setStatus(STATUSES.LOADING));
 //     try {
-//       const res = await fetch("https://fakestoreapi.com/products");
-//       const data = await res.json();
+// const res = await fetch("https://fakestoreapi.com/products");
+// const data = await res.json();
 //       dispatch(setProducts(data));
 //       dispatch(setStatus(STATUSES.IDLE));
 //     } catch (error) {
